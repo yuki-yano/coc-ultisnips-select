489c808f48a5668e063a60c77b96904d6d2c8427
yuki_yano <yuki.ycino@gmail.com>
2021-08-21 18:08:21 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# coc-ultisnips-select
+
+## Install
+
+`:CocInstall coc-ultisnips-select`
+
+## License
+
+MIT
+
+---
+
+> This extension is built with [create-coc-extension](https://github.com/fannheyward/create-coc-extension)
---
+++ src/index.ts
@@ -0,0 +1,57 @@
+import { CompleteResult, events, ExtensionContext, sources, VimCompleteItem, workspace } from 'coc.nvim';
+import fs from 'fs';
+
+type Snippets = {
+  [word: string]: {
+    location: string;
+    description: string;
+  };
+};
+
+export const activate = async (context: ExtensionContext): Promise<void> => {
+  context.subscriptions.push(
+    sources.createSource({
+      name: 'ultisnips source',
+      doComplete: async () => {
+        const items = await getCompletionItems();
+        return items;
+      },
+    })
+  );
+
+  events.on('CompleteDone', async (item: VimCompleteItem) => {
+    if (item.user_data !== 'ultisnips') {
+      return;
+    }
+
+    await workspace.nvim.call('UltiSnips#ExpandSnippet', []);
+  });
+};
+
+const getCompletionItems = async (): Promise<CompleteResult> => {
+  await workspace.nvim.call('UltiSnips#SnippetsInCurrentScope', [1]);
+  const snippets = (await workspace.nvim.getVar('current_ulti_dict_info')) as Snippets;
+
+  return {
+    items: Object.entries(snippets).map(([word, info]) => {
+      const [filePath, lineNumber] = info.location.split(':');
+      const lines = fs.readFileSync(filePath).toString().split('\n') as Array<string>;
+
+      let snipText: Array<string> = [];
+
+      for (const line of lines.slice(Number(lineNumber))) {
+        if (line === 'endsnippet') {
+          break;
+        }
+        snipText = [...snipText, line];
+      }
+
+      return {
+        word,
+        info: `[${info.description}]\n\n${snipText.join('\n')}`,
+        menu: '[ultisnips]',
+        user_data: 'ultisnips',
+      };
+    }),
+  };
+};
